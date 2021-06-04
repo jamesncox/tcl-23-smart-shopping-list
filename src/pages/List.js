ee792573e08ab50d1d055b88eed5c27187019bbc
jamesncox <jamesncox@yahoo.com>
2021-06-04 17:43:28 -0500

    Create FrequencyList and import to List.js

--- src/pages/List.js
+++ src/pages/List.js
@@ -9,6 +9,7 @@ import Button from '../components/Button';
 import IconButton from '../components/IconButton';
 import writingToken from './../img/writingToken.png';
 import { Redirect } from 'react-router';
+import FrequencyList from '../components/FrequencyList';
 
 export default function List({ token }) {
   const history = useHistory();
@@ -242,62 +243,15 @@ export default function List({ token }) {
   const renderUnorderedList = (doc, color) => {
     return (
       <div className="flex items-center" key={doc.id}>
-        <li
-          key={doc.id}
-          className="container flex items-center bg-gray-900 bg-opacity-60 md:font-medium my-1 p-2 rounded w-full"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`h-8 w-8 md:h-10 md:w-10 mr-1 fill-current ${color}`}
-            viewBox="0 0 20 20"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <input
-            type="checkbox"
-            className="mx-2 h-4 w-4 rounded h-5 w-5 bg-black bg-opacity-20 text-gray-700 cursor-pointer"
-            id={doc.id}
-            defaultChecked={
-              doc.data().checked &&
-              compareTimeStampsAndUncheckAfter24Hours(doc, doc.id)
-            }
-            onClick={(e) => markItemPurchased(doc.id, doc.data())}
-          />
-
-          <label htmlFor={doc.id}>
-            <p
-              aria-label={doc.data().item_name}
-              id={doc.id}
-              className="text-md md:text-lg cursor-pointer"
-            >
-              {doc.data().item_name}
-            </p>
-          </label>
-          <button
-            className="ml-auto"
-            key={doc.id}
-            onClick={() => deleteItem(doc)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mx-2 hover:text-red-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
-          </button>
-        </li>
+        <FrequencyList
+          doc={doc}
+          color={color}
+          markItemPurchased={markItemPurchased}
+          compareTimeStampsAndUncheckAfter24Hours={
+            compareTimeStampsAndUncheckAfter24Hours
+          }
+          deleteItem={deleteItem}
+        />
       </div>
     );
   };
