0e584523b8315b78632fac5406c760ccc53ad106
jamesncox <jamesncox@yahoo.com>
2021-06-04 11:54:33 -0500

    Update navbar colors and hover actions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ export default function NavBar(props) {
   return (
     <nav className="grid grid-cols-3 bg-blue-ncs p-5 text-lg md:text-xl w-full divide-x-2 divide-gray-200 fixed bottom-0 z-10 md:px-36 lg:px-48 xl:px-72 lg:w-2/3">
       <RouterLink
-        className="hover:text-green-300"
+        className="hover:underline "
         exact
-        activeClassName="text-green-300 font-bold"
+        activeClassName="text-black underline font-bold"
         to="/list"
       >
         <span
@@ -32,9 +32,9 @@ export default function NavBar(props) {
         </span>
       </RouterLink>
       <RouterLink
-        className="hover:text-green-300"
+        className="hover:underline "
         exact
-        activeClassName="text-green-300 font-bold"
+        activeClassName="text-black underline font-bold"
         to="/add-item"
       >
         <span
@@ -60,9 +60,9 @@ export default function NavBar(props) {
         </span>
       </RouterLink>
       <RouterLink
-        className="hover:text-green-300"
+        className="hover:underline "
         exact
-        activeClassName="text-green-300 font-bold"
+        activeClassName="text-black underline font-bold"
         to="/info"
       >
         <span
