55841a43b779818e6ccd070c9393b9a640bb2bc9
jamesncox <jamesncox@yahoo.com>
2021-06-05 11:12:05 -0500

    Add toggleEditable for Edit or Done button

--- src/components/SortableFilters.js
+++ src/components/SortableFilters.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+
 export default function SortableFilters({
   listItems,
   renderSortableList,
   filterByAlphabetizedStoreOrder,
 }) {
+  const [editable, setEditable] = useState(false);
+
+  const toggleEditable = () => {
+    if (editable) {
+      setEditable(false);
+    } else {
+      setEditable(true);
+    }
+  };
+
   return (
     <>
       {filterByAlphabetizedStoreOrder(listItems).length !== 0 && (
         <div className="flex items-center full mt-5 mb-1">
           <span className="text-xl md:text-2xl font-light">shopping order</span>
-          <button className="w-12 ml-auto bg-gray-900 bg-opacity-50 rounded p-2 hover:bg-gray-700">
-            Edit
+          <button
+            className="w-16 ml-auto bg-gray-900 bg-opacity-50 rounded p-2 hover:bg-gray-700"
+            onClick={toggleEditable}
+          >
+            {editable ? 'Done' : 'Edit'}
           </button>
         </div>
       )}
