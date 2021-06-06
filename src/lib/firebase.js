081e125104f6f96bc35e04615786866cdddb834d
jamesncox <jamesncox@yahoo.com>
2021-06-06 12:04:53 -0500

    Create process.env and create secret key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-RVe8Icisr03G002oOc9Un7GXFjIKe-M',
-  authDomain: 'tcl-23-shopping-list.firebaseapp.com',
-  databaseURL: 'https://tcl-23-shopping-list-default-rtdb.firebaseio.com',
-  projectId: 'tcl-23-shopping-list',
-  storageBucket: 'tcl-23-shopping-list.appspot.com',
-  messagingSenderId: '17231198664',
-  appId: '1:17231198664:web:180626daef3992d3cfe8fd',
+  apiKey: `{REACT_APP_FIREBASE_KEY}`,
+  authDomain: 'honey-do-d3592.firebaseapp.com',
+  projectId: 'honey-do-d3592',
+  storageBucket: 'honey-do-d3592.appspot.com',
+  messagingSenderId: '118743555342',
+  appId: '1:118743555342:web:c90ccee22cc84a2064cb5f',
+  measurementId: 'G-NF5SZ88B3G',
 };
 
 // Firebase configuration for the App
